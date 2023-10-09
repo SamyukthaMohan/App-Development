bc5a344fc3f986de7ef8199cfe062cb4c1b2bed3
Samyuktha A M <110532180+SamyukthaMohan@users.noreply.github.com>
2023-10-09 11:37:48 +0530

    Delete Day 5 Backend directory

--- Day 5 Backend/repo/SignupRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.SignupModel;
-
-public interface SignupRepo extends JpaRepository<SignupModel, Integer>{
-
-}
