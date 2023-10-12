ed0d1d75f767a32cd9186a48e6345e49c82c5e4e
Samyuktha A M <110532180+SamyukthaMohan@users.noreply.github.com>
2023-10-12 15:27:58 +0530

    Add files via upload

---
+++ Day 7 Backend/playlist/model/PlaylistModel.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="story_playlist")
+public class PlaylistModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int userid;
+	private int storyid;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getStoryid() {
+		return storyid;
+	}
+	public void setStoryid(int storyid) {
+		this.storyid = storyid;
+	}
+	
+	
+}
